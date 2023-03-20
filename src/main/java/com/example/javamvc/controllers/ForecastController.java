1fcc1d837daf164dca32ebb7c549f864a6f19e31
gintare123 <gintare.ragaisiene@gmail.com>
2023-03-20 08:23:59 +0200

    commit JavaMVC

--- src/main/java/com/example/javamvc/controllers/ForecastController.java
+++ src/main/java/com/example/javamvc/controllers/ForecastController.java
@@ -1,5 +1,6 @@
 package com.example.javamvc.controllers;
 
+import com.example.javamvc.models.IndexModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,11 +14,16 @@ public class ForecastController {
     public ModelAndView index() {
         var modelAndView = new ModelAndView("index");
 
+        var indexModel = new IndexModel();
+
         var cities = new ArrayList<String>();
         cities.add("Vilnius");
         cities.add("Kaunas");
 
-        modelAndView.addObject("cities", cities);
+        indexModel.cities = cities;
+
+
+        modelAndView.addObject("IndexModel", indexModel);
 
         return  modelAndView;
     }
---
+++ src/main/java/com/example/javamvc/models/IndexModel.java
@@ -0,0 +1,8 @@
+package com.example.javamvc.models;
+
+import java.util.ArrayList;
+
+public class IndexModel {
+    public ArrayList<String> cities;
+    public ArrayList<ForecastModel> forecasts;
+}
