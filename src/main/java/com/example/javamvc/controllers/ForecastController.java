d604395adec04a96bbab942097e947820532ceac
gintare123 <gintare.ragaisiene@gmail.com>
2023-03-20 09:38:23 +0200

    model basic

--- src/main/java/com/example/javamvc/controllers/ForecastController.java
+++ src/main/java/com/example/javamvc/controllers/ForecastController.java
@@ -1,5 +1,6 @@
 package com.example.javamvc.controllers;
 
+import com.example.javamvc.models.ForecastModel;
 import com.example.javamvc.models.IndexModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,15 @@ public class ForecastController {
         var cities = new ArrayList<String>();
         cities.add("Vilnius");
         cities.add("Kaunas");
-
         indexModel.cities = cities;
 
+        var forecats = new ArrayList<ForecastModel>();
+        forecats.add(new ForecastModel("2023.03.18 12:00", 2));
+        forecats.add(new ForecastModel("2023.03.18 13:00", 2));
+        forecats.add(new ForecastModel("2023.03.19 12:00", 10));
+        indexModel.forecasts = forecats;
 
         modelAndView.addObject("IndexModel", indexModel);
-
         return  modelAndView;
     }
 
